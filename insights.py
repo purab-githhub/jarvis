26ae699a4104cfd8f72028616b1102d06d1b7fd4
purab rupesh agarwal <agarwal1purab1@gmail.com>
2026-09-03 08:45:52 +0530

    Add planner insights entry point

---
+++ insights.py
@@ -0,0 +1,7 @@
+from database import initialize_database
+from planner_insights import print_planner_insights
+
+
+if __name__ == "__main__":
+    initialize_database()
+    print_planner_insights()
